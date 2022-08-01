859018fcf63d738964cdd7962753be5ed4fc739f
xhres07 <70744753+xhres07@users.noreply.github.com>
2022-08-01 09:34:53 +0545

    first commit

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,52 @@
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function WidgetSm() {
+  const [newUsers, setNewUsers] = useState([]);
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get("/users?new=true", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWViOTAyN2I3ZWI1ZDE0ZTgyMGJiZCIsImlzQWRtaW4iOiJ0cnVlIiwiaWF0IjoxNjQ0NzU4MzIzLCJleHAiOjE2NDUzNjMxMjN9.PVW5dQEO4Zj98hY--wIUumW3m3WanySeSiA59J9LkCM",
+          },
+        });
+        setNewUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNewUsers();
+  }, []);
+  
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {newUsers.map((user) => (
+          <li className="widgetSmListItem">
+            <img
+              src={
+                user.profilePic ||
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/context/authContext/AuthActions.js
@@ -0,0 +1,19 @@
+export const loginStart = () =>({
+    type:"LOGIN_START",
+});
+
+export const loginSuccess = (user) =>({
+    type:"LOGIN_SUCCESS",
+    payload: user, 
+});
+
+export const loginFailure = () =>({
+    type:"LOGIN_FAILURE",
+});
+
+//LOGOUT
+
+export const logout = () =>({
+    type:"LOGOUT",
+});
+
